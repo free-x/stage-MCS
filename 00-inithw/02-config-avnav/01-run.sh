738bb353f130b672359aae0cab50cae02f9e4d5c
free-x <oroitburd@gmail.com>
2020-08-13 15:52:54 +0200

    correct avnav user config

--- 00-inithw/02-config-avnav/01-run.sh
+++ 00-inithw/02-config-avnav/01-run.sh
@@ -2,3 +2,6 @@
 
 #install MCS template
 install -m 644 files/avnav_server.xml "${ROOTFS_DIR}/usr/lib/avnav/raspberry/"
+
+#install for user
+install -m 644 files/avnav_server.xml "${ROOTFS_DIR}/home/pi/avnav/data/"
